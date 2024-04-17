05bd4ef78773137e9ddc23b1110ba6ca046e7212
Anubhav Singh <anubhavsinghgtm@gmail.com>
2024-04-17 16:55:18 +0530

    UI is added and working fine now

--- script.js
+++ script.js
@@ -10,20 +10,25 @@
 */
 
 
+let playerPoints = 0;
+let computerPoints = 0;
+let gameScore = 5;
+
+
 function getComputerChoice(choices){
     return choices[Math.floor(Math.random()*3)];
 }
 
-function getUserChoice(choices){
-    console.log("Enter Your Choice(1, 2, 3 or 0 for quiting):");
-    console.log(" 1. For Rock \n 2. For Paper \n 3. For Scissor \n 0. Quit Game" );
-    let choice = prompt("Enter your choice: ");
-    if(choice == 0){
-        console.log("Game Over");
-        return 0;
-    }
-    return choices[choice-1];
-}
+// function getUserChoice(choices){
+//     console.log("Enter Your Choice(1, 2, 3 or 0 for quiting):");
+//     console.log(" 1. For Rock \n 2. For Paper \n 3. For Scissor \n 0. Quit Game" );
+//     let choice = prompt("Enter your choice: ");
+//     if(choice == 0){
+//         console.log("Game Over");
+//         return 0;
+//     }
+//     return choices[choice-1];
+// }
 
 function calculateWinner(computerChoice, userChoice){
 
@@ -45,54 +50,95 @@ function calculateWinner(computerChoice, userChoice){
 
 
 
-function showResult(winner){
-    if(winner == 2)
-        console.log("Match is Draw!");
-    else if(winner == 1) 
-        console.log("You win this round!");
-    else
-        console.log("Computer wins this round!");
+
+function updateResult(winner){
+    const playerScore = document.querySelector('#player-score');
+    const computerScore = document.querySelector('#computer-score');
+    const currentWinner = document.querySelector(".current-winner");
+
+    if(winner === 0){
+        computerPoints++;
+        computerScore.innerHTML = computerPoints;
+        currentWinner.innerHTML = "Ohh! You lose!";
+    }
+    else if(winner === 1){
+        playerPoints++;
+        playerScore.innerHTML = playerPoints;
+        currentWinner.innerHTML = "Yeah, You win!";
+    }
+    else{
+        currentWinner.innerHTML = "Haha! You both choose the same"
+    }
 }
 
-async function playGame(gameTime){
-    
-    let userPoints = 0;
-    let computerPoints = 0;
 
+async function showChoices(computerChoice, playerChoice){
+    const currentResultSection = document.querySelector(".current-result");
+    const playerChoiceEle = document.querySelector(".player-choice");
+    const computerChoiceEle = document.querySelector(".computer-choice");
+    
+    currentResultSection.style.visibility = "visible";
+    
+    playerChoiceEle.innerHTML = "You: <strong>" + playerChoice + "<strong>";
+    computerChoiceEle.innerHTML = "Computer: <strong>" + computerChoice + "<strong>";
+
+    await new Promise(resolve => setTimeout(resolve, 3500));
+    currentResultSection.style.visibility = 'hidden';
+}
+
+
+function stopGame(){
+    const currentResultSection = document.querySelector(".current-result");
+    const gamePlayingSection = document.querySelector(".game-time");
+    gamePlayingSection.style.display = "none";
+    currentResultSection.style.display = "none"
+    
+}
+
+function showWinner(winner){
+    const finalResultEle = document.querySelector(".final-result");
+    finalResultEle.innerHTML = " "+ winner + " wins!!!"
+}
+
+async function playGame(userChoice){
+    
     let finalWinner;
     
     const choices = ['rock', 'paper', 'scissor'];
 
-    while(gameTime--){
-        
-        const computerChoice = getComputerChoice(choices);
-        const userChoice = getUserChoice(choices);
-        
-        if(userChoice === 0) 
-            break;
+    const computerChoice = getComputerChoice(choices);
+    
+    showChoices(computerChoice, userChoice);
 
-        let winner = calculateWinner(computerChoice, userChoice);
+    let winner = calculateWinner(computerChoice, userChoice);
         
-        showResult(winner);
-        
-        // points updation 
-        if(winner == 0) computerPoints++;
-        else if(winner == 1) userPoints++;
+    updateResult(winner);
 
-        console.log("Your Points: " + userPoints + " | Computer Points: " + computerPoints);
-        console.log("====================================================");
-        await new Promise(resolve => setTimeout(resolve, 500));
-    }
+    await new Promise(resolve => setTimeout(resolve, 500));
 
-    // result declaration
-    if(userPoints > computerPoints){
-        console.log('Hurrah! You have won the match!');
-    } 
-    else if(computerPoints > userPoints){
-        console.log("Alas! You lose the game");
-    }
-    else{
-        console.log("OHH! You played pretty well but Game is Draw");
+    if(playerPoints >= 5 || computerPoints >= 5){
+        stopGame();
+        if(playerPoints > computerPoints){
+            showWinner("Hurrah! You");
+        }
+        else{
+            showWinner("Ohh! Computer");
+        }
     }
 
 }
+
+
+// Dom manipulation 
+
+const startBtn = document.querySelector(".start-game-btn");
+const startGameSection = document.querySelector(".start-game");
+
+const gamePlayingSection = document.querySelector(".game-time");
+const scoreCardSection = document.querySelector(".score-area");
+
+startBtn.addEventListener('click',() => {
+    startGameSection.style.display = "none";
+    gamePlayingSection.style.display = "flex";
+    scoreCardSection.style.display = "block"; 
+})
